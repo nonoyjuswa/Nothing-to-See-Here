621fe0bc889839c4caa60b5674609a3336b7dec6
Joshua <shinjouakane2@gmail.com>
2026-02-23 21:59:56 +0800

    inputs

--- people_memory_app/view/main_window.py
+++ people_memory_app/view/main_window.py
@@ -1,4 +1,13 @@
-from PySide6.QtWidgets import QMainWindow, QLabel
+from PySide6.QtWidgets import (
+    QMainWindow, 
+    QLabel,
+    QWidget,
+    QLineEdit,
+    QTextEdit,
+    QPushButton,
+    QVBoxLayout,
+    QFormLayout
+    )
 from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
@@ -8,7 +17,23 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("People Memory App")
         self.setMinimumSize(600, 400)
 
-        label = QLabel("App Initialized")
-        label.setAlignment(Qt.AlignCenter)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        self.setCentralWidget(label)
+        main_layout = QVBoxLayout()
+        central_widget.setLayout(main_layout)
+
+        form_layout = QFormLayout()
+
+        self.name_input = QLineEdit()
+        self.where_met_input = QLineEdit()
+        self.notes_input = QTextEdit()
+
+        form_layout.addRow("Name:", self.name_input)
+        form_layout.addRow("Where Met:", self.where_met_input)
+        form_layout.addRow("Notes:", self.notes_input)
+
+        main_layout.addLayout(form_layout)
+
+        self.add_button = QPushButton("Add Person")
+        main_layout.addWidget(self.add_button)
